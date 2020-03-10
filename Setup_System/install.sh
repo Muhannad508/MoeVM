2b9e730b0527c3e3346319448ed3c4e7810e34e3
Muhannad <Muhannad508@gmail.com>
2020-03-11 01:44:28 +0300

    Add files via upload

---
+++ Setup_System/install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+apt-get update
+
+
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    
+apt-get install -y git
+#apt-get install -y openjdk-13-jdk
+
+apt-get install -y flatpak
+apt-get install -y gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+ #curl -s "https://get.sdkman.io" | bash
+ #source $HOME"/.sdkman/bin/sdkman-init.sh"
+
+#sdk install java \
+#sdk install maven \
+#sdk install gradle \
+#sdk install springboot
+
+sudo snap install code --classic
+sudo snap install skype --classic
+
+
+
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /home/moe/Downloads
+
+echo "dev tools have been installed , you shall begin , my lord "
