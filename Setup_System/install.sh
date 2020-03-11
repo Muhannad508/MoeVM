29975b27b675e0d6082a347024f0c6ffd69dd708
muhannad508 <muhannad508@gmail.com>
2020-03-11 12:51:10 +0300

    Changes to be committed: 	modified:   Setup_System/install.sh

--- Setup_System/install.sh
+++ Setup_System/install.sh
@@ -17,20 +17,25 @@ apt-get install -y flatpak
 apt-get install -y gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
- #curl -s "https://get.sdkman.io" | bash
- #source $HOME"/.sdkman/bin/sdkman-init.sh"
-
-#sdk install java \
-#sdk install maven \
-#sdk install gradle \
-#sdk install springboot
-
-sudo snap install code --classic
-sudo snap install skype --classic
+# curl -s "https://get.sdkman.io" | bash
+# $ source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 
+ sdk install java 
+ sdk install maven 
+ sdk install gradle 
+ sdk install springboot
 
+snap install code --classic
+snap install skype --classic
+ snap install gitkraken
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /home/moe/Downloads
+sudo add-apt-repository ppa:amanusk/python-s-tui
+sudo apt-get update
+sudo apt-get install python3-s-tui
+
+apt-get install htop
+
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /home/moe/Downloads
 
 echo "dev tools have been installed , you shall begin , my lord "
