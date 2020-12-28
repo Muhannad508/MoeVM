353b0d7f9c4dfabce08a8d9c3d46e72d5065cd5b
Muhannad <Muhannad508@gmail.com>
2020-12-28 14:51:17 +0300

    Add files via upload

---
+++ scripts/ubuntu_Setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+print_status() {
+    echo
+    echo "## $1"
+    echo
+}
+
+
+print_bold() {
+    title="$1"
+    text="$2"
+
+    echo
+    echo "${red}================================================================================${normal}"
+    echo "${red}================================================================================${normal}"
+    echo
+    echo -e "  ${bold}${yellow}${title}${normal}"
+    echo
+    echo -en "  ${text}"
+    echo
+    echo "${red}================================================================================${normal}"
+    echo "${red}================================================================================${normal}"
+}
+
+
+bail() {
+    echo 'Error executing command, exiting'
+    exit 1
+}
+
+exec_cmd_nobail() {
+    echo "+ $1"
+    bash -c "$1"
+}
+
+exec_cmd() {
+    exec_cmd_nobail "$1" || bail
+}
+
+PRE_INSTALL_PKGS=""
+
+# Populating Cache
+print_status "Populating apt-get cache..."
+exec_cmd 'apt-get update'
+
+ print_status "Installing packages required for setup..."
+    # This next command needs to be redirected to /dev/null or the script will bork
+    # in some environments
+    PRE_INSTALL_PKGS="htop"
+
+    exec_cmd "apt-get install -y ${PRE_INSTALL_PKGS} > /dev/null 2>&1"
