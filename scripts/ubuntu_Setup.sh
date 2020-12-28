f49917393345b051d1fb750b52422b41ee6e817e
Muhannad <Muhannad508@gmail.com>
2020-12-28 16:43:00 +0300

    Add files via upload

--- scripts/ubuntu_Setup.sh
+++ scripts/ubuntu_Setup.sh
@@ -7,6 +7,25 @@ print_status() {
     echo
 }
 
+if test -t 1; then # if terminal
+    ncolors=$(which tput > /dev/null && tput colors) # supports color
+    if test -n "$ncolors" && test $ncolors -ge 8; then
+        termcols=$(tput cols)
+        bold="$(tput bold)"
+        underline="$(tput smul)"
+        standout="$(tput smso)"
+        normal="$(tput sgr0)"
+        black="$(tput setaf 0)"
+        red="$(tput setaf 1)"
+        green="$(tput setaf 2)"
+        yellow="$(tput setaf 3)"
+        blue="$(tput setaf 4)"
+        magenta="$(tput setaf 5)"
+        cyan="$(tput setaf 6)"
+        white="$(tput setaf 7)"
+    fi
+fi
+
 
 print_bold() {
     title="$1"
@@ -39,22 +58,47 @@ exec_cmd() {
     exec_cmd_nobail "$1" || bail
 }
 
+install() {
+    PKG="$1"
+     print_status " ${bold} Installing ${PKG} ..."
+    exec_cmd "apt-get install -y ${PKG}"
+     print_status "${bold}${green} ${PKG} has been installed"
+
+}
+
+setup_node() {
 PRE_INSTALL_PKGS=""
 
 # Using Debian, as root
 exec_cmd 'curl -sL https://deb.nodesource.com/setup_lts.x | bash -'
 PRE_INSTALL_PKGS="nodejs"
 exec_cmd "apt-get install -y ${PRE_INSTALL_PKGS}"
- print_status "Nodejs has been Installed..."
+ 
+ print_status "Installing some npm packages..."
+npm install -g tldr
+npm install -g @vue/cli
+npm install -g json-server
+npm install -g http-server
+}
 
+setup_snap() {
+exec_cmd 'snap install cheat'
+exec_cmd 'snap install authy --classic'
+}
+
+
+#install 'htop'
+
+CODE="$PWD/code.deb"
+
+exec_cmd 'curl -L https://code.visualstudio.com/sha/download\?build\=stable\&os\=linux-deb-x64 -o code.deb'
+print_status "location deb file ${CODE}"
+exec_cmd "apt-get install ${CODE}"
 
 # Populating Cache
 # print_status "Populating apt-get cache..."
 # exec_cmd 'apt-get update'
 
- print_status "Installing packages required for setup..."
-    # This next command needs to be redirected to /dev/null or the script will bork
-    # in some environments
-    PRE_INSTALL_PKGS="htop"
 
-    exec_cmd "apt-get install -y ${PRE_INSTALL_PKGS} > /dev/null 2>&1"
+  #  exec_cmd "apt-get install -y ${PRE_INSTALL_PKGS} > /dev/null 2>&1"
+
